6d87c471e24bd8a774bdfe4e6eabf2e4e91103ff
Nima Rahrakhshan <Nima.Rahrakhshan@gmail.com>
2018-10-28 16:06:04 +0100

    XCode Project Push

---
+++ XCode/DewertOkin/DewertOkin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DewertOkin
+//
+//  Created by Nima Rahrakhshan on 28.10.18.
+//  Copyright © 2018 Team DewertOkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
