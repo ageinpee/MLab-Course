718e5e297e284dba958bd58b53edad1b3819f54d
Jan Robert <jan.janneck@outlook.com>
2018-11-07 12:29:55 +0100

    Embedded SettingsView in NavigationController

--- XCode/DewertOkin/DewertOkin/SettingTableViewController.swift
+++ XCode/DewertOkin/DewertOkin/SettingTableViewController.swift
@@ -53,17 +53,17 @@ class SettingTableViewController: UITableViewController {
         }
         
         if (indexPath.row == settingsEntries.firstIndex(of: .deviceInfo)) {
-            cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+            cell.accessoryType = .disclosureIndicator
         } else if (indexPath.row == settingsEntries.firstIndex(of: .profiles)) {
-            cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+            cell.accessoryType = .disclosureIndicator
         } else if (indexPath.row == settingsEntries.firstIndex(of: .achievements)) {
-            cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+            cell.accessoryType = .disclosureIndicator
         } else if (indexPath.row == settingsEntries.firstIndex(of: .warranty)) {
-            cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+            cell.accessoryType = .disclosureIndicator
         } else if (indexPath.row == settingsEntries.firstIndex(of: .about)) {
-            cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+            cell.accessoryType = .disclosureIndicator
         } else if (indexPath.row == settingsEntries.firstIndex(of: .siri)) {
-            cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+            cell.accessoryType = .disclosureIndicator
         }
         
         // Configure the cell...
