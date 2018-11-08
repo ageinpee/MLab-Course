bb55b14eff1aefbde01d5658d510d8e165ac6189
6peters <6peters@rzmac34.informatik.uni-hamburg.de>
2018-11-08 15:08:22 +0100

    added first 2 functions to reminder

--- XCode/DewertOkin/DewertOkin/Presets-Reminder-Timer/Reminder.swift
+++ XCode/DewertOkin/DewertOkin/Presets-Reminder-Timer/Reminder.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+import CoreData
+
+class Reminder {
+    var reminderName : String = ""
+    var reminderDescription : String = ""
+    var reminderTime : Int = 0
+    
+    func ´init´(){
+    
+    }
+    
+    /**
+     # saving a new reminder-entity into the database
+     Parameters:
+     - withName: String --> Name of the Reminder
+     - withDescription: String --> Description of the Reminder
+     - atTime: Int --> Time when the Reminder should be activated
+    */
+    func saveNewReminder(withName: String, withDescription: String, atTime: Int) {
+        let context = PersistenceService.persistentContainer.viewContext
+        let entityName = "Reminder" //Entity in the Datamodel
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: context) else {
+            return
+        }
+        let newReminder = NSManagedObject(entity: entity, insertInto: context)
+        
+        newReminder.setValue(withName, forKey: "reminderName")
+        newReminder.setValue(withDescription, forKey: "reminderDescription")
+        newReminder.setValue(atTime, forKey: "reminderTime")
+        
+        //save data
+        do {
+            try context.save()
+            print("Saved: reminderName '\(withName)', reminderDescription '\(withDescription)', reminderTime '\(atTime)'")
+        } catch {
+            print(error)
+        }
+    }
+    
+    
+    /**
+     #fetching a reminders name by name
+     Parameters:
+     - withName: String --> the name of the reminder you want to fetch
+    */
+    func fetchAttribute(withName: String) {
+        let context = PersistenceService.persistentContainer.viewContext
+        let entityName = "Reminder" //Entity in the Datamodel
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        request.predicate = NSPredicate(format: "reminderName = %@", withName)
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "reminderName") as! String)
+            }
+        } catch {
+            print("Failed")
+        }
+    }
+    
+}
