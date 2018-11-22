093d273d0d9a39de619cbc15585040887d3170b3
Danial Bagheri <d.bagheri1986@gmail.com>
2018-11-22 18:10:33 +0100

    Correct bluetoothCode

--- XCode/DewertOkin/DewertOkin/BluetoothViewController.swift
+++ XCode/DewertOkin/DewertOkin/BluetoothViewController.swift
@@ -13,31 +13,36 @@ class BluetoothViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     
     var centralManager: CBCentralManager!
     var dwertOkinControllerPeripheral: CBPeripheral!
-    var pickerData: [String] = [String]()
+    var dataSource: [CBPeripheral] = [CBPeripheral]()
     @IBOutlet weak var devicePicker: UIPickerView!
-
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
+        return dataSource.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row]
+        let deviceName = dataSource[row].name ?? ""
+        return deviceName
     }
-
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("--------change option-------")
+        if (dataSource.count > 0) {
+            dwertOkinControllerPeripheral = dataSource[row];
+            dwertOkinControllerPeripheral.delegate = self
+        }
+    }
+    
     override func viewDidLoad() {
         print("--------viewDidLoad-------")
         super.viewDidLoad()
         centralManager = CBCentralManager(delegate: self, queue: nil)
         devicePicker.delegate = self
         devicePicker.dataSource = self
-//        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
-        let colors = ["Red","Yellow","Green","Blue"]
-
         // Do any additional setup after loading the view.
     }
     
@@ -49,7 +54,11 @@ class BluetoothViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     
     @IBOutlet weak var label1: UILabel!
     @IBAction func btn1(_ sender: Any) {
+        print("----btn pressed------")
         
+        centralManager.stopScan()
+        centralManager.connect(dwertOkinControllerPeripheral)
+        dwertOkinControllerPeripheral.discoverServices(nil);
         label1.text = "-----";
     }
 }
@@ -75,22 +84,41 @@ extension BluetoothViewController: CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-//        if (peripheral.identifier.uuidString == "153222D8-A953-2297-1EC4-30436ED0D5A3"){
-            print("----dada------")
-            pickerData.append(peripheral.identifier.uuidString)
-            devicePicker.reloadAllComponents()
-
-            print(peripheral)
+        
+        print("----dada------")
+        print(peripheral.name ?? "")
+        let deviceName = peripheral.name ?? ""
+        if (isOkinDevice(name:deviceName)){
             dwertOkinControllerPeripheral = peripheral;
             dwertOkinControllerPeripheral.delegate = self
-            centralManager.stopScan()
-            centralManager.connect(dwertOkinControllerPeripheral)
-//        }
+            dataSource.append(peripheral)
+            devicePicker.reloadAllComponents()
+        }
+        
+        
+        
+        //            print(peripheral)
+        //            dwertOkinControllerPeripheral = peripheral;
+        //            dwertOkinControllerPeripheral.delegate = self
+        //            centralManager.stopScan()
+        //            centralManager.connect(dwertOkinControllerPeripheral)
+        //        }
     }
+    
+    
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("connect")
         dwertOkinControllerPeripheral.discoverServices(nil);
+    }
+    
+    func isOkinDevice(name: String) -> Bool {
+        if (name.lowercased().range(of:"okin") != nil) {
+            print("is okin")
+            return true;
+        }
+        
+        return false;
     }
 }
 
@@ -113,13 +141,109 @@ extension BluetoothViewController: CBPeripheralDelegate {
         }
     }
     
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService,
-                    error: Error?) {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         guard let characteristics = service.characteristics else { return }
         
         for characteristic in characteristics {
             print("-------- chars ---------")
             print(characteristic)
+            //            if (characteristic.uuid.uuidString == "1720") {
+            //                print("-------- if done ---------")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            //
+            //            if (characteristic.uuid.uuidString == "62741625-52F9-8864-B1AB-3B3A8D65950B") {
+            //                print("-------- if done ---------")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            //
+            //            if (characteristic.uuid.uuidString == "662741525-52F9-8864-B1AB-3B3A8D65950") {
+            //                print("-------- if done ---------")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            ////
+            //            if (characteristic.uuid.uuidString == "1721") {
+            //                print("TTTTTTTTTTTTTTTTTTT if doneTTTTTTTTT")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            //
+            //            if (characteristic.uuid.uuidString == "1821") {
+            //                print("************** if done **********")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            //
+            //            if (characteristic.uuid.uuidString == "1820") {
+            //                print("%%%%%%%% if done %%%%%%%%%%%")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            //
+            //            if (characteristic.uuid.uuidString == "90311725-25FA-3346-12EF-3CFB7A2556AC") {
+            //                print("%%%%%%%% if done %%%%%%%%%%%")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            //
+            //            if (characteristic.uuid.uuidString == "00001534-1212-EFDE-1523-785FEABCD123") {
+            //                print("%%%%%%%% if done %%%%%%%%%%%")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            
+            //Turns on the lamp
+            if (characteristic.uuid.uuidString == "00001532-1212-EFDE-1523-785FEABCD123") {
+                print("%%%%%%%% if done %%%%%%%%%%%")
+                let bytes : [UInt8] = [ 0x04, 0x04, 0x000 ]
+                let data = Data(bytes:bytes)
+                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withResponse)
+            }
+            //
+            //            if (characteristic.uuid.uuidString == "00001531-1212-EFDE-1523-785FEABCD123") {
+            //                print("%%%%%%%% if done %%%%%%%%%%%")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            ////
+            //            if (characteristic.uuid.uuidString == "90311625-25FA-3346-12EF-3CFB7A2556AC") {
+            //                print("%%%%%%%% if done %%%%%%%%%%%")
+            //
+            //                let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //                let data = Data(bytes:bytes)
+            //                peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            //            }
+            
+            
+            //Turn lamp on
+            //            let bytes : [UInt8] = [ 0x00, 0x04, 0x000 ]
+            //            let data = Data(bytes:bytes)
+            //            peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withoutResponse)
+            
+            //            let bytes : [UInt8] = [ 0x04, 0x02, 0x00,0x02, 0x00, 0x00]
+            //            let data = Data(bytes:bytes)
+            //            peripheral.writeValue(data, for: characteristic, type:  CBCharacteristicWriteType.withResponse)
             
             //            if characteristic.properties.contains(.read) {
             //                print("\(characteristic.uuid): properties contains .read")
@@ -128,16 +252,15 @@ extension BluetoothViewController: CBPeripheralDelegate {
             //                print("\(characteristic.uuid): properties contains .notify")
             //            }
             
-            peripheral.readValue(for: characteristic)
+            //            peripheral.readValue(for: characteristic)
             //            peripheral.writeValue(0x00040000, for: characteristic)
             
         }
     }
     
-    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic,
-                    error: Error?) {
-        print("char valueeeeeee")
-        print(characteristic.value ?? "no value")
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        print("Replayyyyyyyy 000000))))))))))))))))))))))))))))))")
+        print(characteristic)
     }
     
 }
