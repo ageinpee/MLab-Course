962680ba864002162adc60145cff515f69fff13d
MacBook-Benutzer <macbook@dhcp-172-21-96-153.wlan.uni-hamburg.de.local>
2018-11-29 15:57:44 +0100

    added RemoteController

---
+++ XCode/DewertOkin/DewertOkin/RemoteControl/RemoteController.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteController.swift
+//  DewertOkin
+//
+//  Created by MacBook-Benutzer on 29.11.18.
+//  Copyright © 2018 Team DewertOkin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class RemoteController: UIViewController{
+
+}
+
