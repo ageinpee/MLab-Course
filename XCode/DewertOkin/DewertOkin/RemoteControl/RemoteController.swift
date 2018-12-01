38612ddc5efd0a4c171376850d628738aa84b6b1
MacBook-Benutzer <macbook@macbook-2.local>
2018-12-01 14:17:39 +0100

    Remote Animation working. inserted Bluetooth placeholders. TODO: attach View to the other views

--- XCode/DewertOkin/DewertOkin/RemoteControl/RemoteController.swift
+++ XCode/DewertOkin/DewertOkin/RemoteControl/RemoteController.swift
@@ -12,19 +12,28 @@ import UIKit
 
 class RemoteController: UIViewController{
     
+    //----------------------------------------
+    //------ Fancy Remote UI-Elements --------
     @IBOutlet weak var PresetsButtonObj: UIButton!
     @IBOutlet weak var AddPresetsButtonObj: UIButton!
     @IBOutlet weak var ExtraFunctionsButtonObj: UIButton!
+    @IBOutlet weak var Image: UIImageView!
+    
     
     //----------------------------------------
-    //--------------- Setup ------------------
+    //------ Fancy Remote Attributes ---------
+    var oldTranslation = 0 //used to define in which direction the old translation was going
+
+    
+    //----------------------------------------
+    //--------- Fancy Remote Setup -----------
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var width = PresetsButtonObj.frame.width
         PresetsButtonObj.layer.cornerRadius = width/2
-        PresetsButtonObj.layer.masksToBounds=true
+        PresetsButtonObj.layer.masksToBounds = true
         PresetsButtonObj.layer.borderWidth = 1
         PresetsButtonObj.layer.borderColor = UIColor.gray.cgColor
         
@@ -36,10 +45,136 @@ class RemoteController: UIViewController{
         
         width = ExtraFunctionsButtonObj.frame.width
         ExtraFunctionsButtonObj.layer.cornerRadius = width/2
-       ExtraFunctionsButtonObj.layer.masksToBounds = true
+        ExtraFunctionsButtonObj.layer.masksToBounds = true
         ExtraFunctionsButtonObj.layer.borderWidth = 1
         ExtraFunctionsButtonObj.layer.borderColor = UIColor.gray.cgColor
+        
+        Image.image = UIImage(named: "ChairNormal")
+        Image.contentMode = .scaleAspectFit
     }
+    
+    
+    //---------------------------------------
+    //----- Fancy Design Remote Actions -----
+    
+    @IBAction func PresetsButton(_ sender: Any) {
+        //shall open presets menu/page/sheet to select a preset
+    }
+    
+    @IBAction func AddPresetsButton(_ sender: Any) {
+        //shall trigger both buttons that are used to save a preset
+    }
+    
+    @IBAction func ExtraFunctions(_ sender: Any) {
+        //maybe a menu with extra functions like massage/ubl/torch etc...
+    }
+    
+    
+    //-------------------------------------------------------------
+    
+    
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        //translation defines the direction of the pan
+        let translation = recognizer.translation(in: self.view)
+        
+        let viewWidth = self.view.frame.width
+        let start = recognizer.location(in: self.view)
+        
+        if start.x <= viewWidth/2 {
+            //-------------------------------------------
+            //-- Status: Began --> Setup-phase for Pan --
+            if recognizer.state == UIGestureRecognizer.State.began {
+                if translation.y < 0 {
+                    oldTranslation = 1
+                    Image.image = UIImage(named: "ChairFeetUp")
+                } else if translation.y >= 0 {
+                    oldTranslation = -1
+                    Image.image = UIImage(named: "ChairFeetDown")
+                }
+            }
+            
+            //-------------------------------------------
+            //-- Status: Changed --> Pan in Action ------
+            else if recognizer.state == UIGestureRecognizer.State.changed {
+                if translation.y < 0 {
+                    if oldTranslation == -1 {
+                        Image.image = UIImage(named: "ChairFeetUp")
+                    }
+                    oldTranslation = 1
+                    
+                    // --> Placeholder <--
+                    // Put Actuator Function for feet up here
+                    // -------------------
+                }
+                
+                else if translation.y >= 0 {
+                    if oldTranslation == +1 {
+                        Image.image = UIImage(named: "ChairFeetDown")
+                    }
+                    oldTranslation = -1
+                    
+                    // --> Placeholder <--
+                    // Put Actuator Function for feet down here
+                    // -------------------
+                }
+            }
+            
+            //--------------------------------------------------------------------------
+            //-- Status: Ended --> Pan has ended. Reestablish starting-condition --
+            else if recognizer.state == UIGestureRecognizer.State.ended {
+                oldTranslation = 0 //set back to start-value
+                Image.image = UIImage(named: "ChairNormal")
+            }
+        }
+        else if start.x > viewWidth/2 {
+            //-------------------------------------------
+            //-- Status: Began --> Setup-phase for Pan --
+            if recognizer.state == UIGestureRecognizer.State.began {
+                if translation.y < 0 {
+                    oldTranslation = 1
+                    Image.image = UIImage(named: "ChairChestUp")
+                } else if translation.y >= 0 {
+                    oldTranslation = -1
+                    Image.image = UIImage(named: "ChairChestDown")
+                }
+            }
+                
+                //-------------------------------------------
+                //-- Status: Changed --> Pan in Action ------
+            else if recognizer.state == UIGestureRecognizer.State.changed {
+                if translation.y < 0 {
+                    if oldTranslation == -1 {
+                        Image.image = UIImage(named: "ChairChestUp")
+                    }
+                    oldTranslation = 1
+                    
+                    // --> Placeholder <--
+                    // Put Actuator Function for feet up here
+                    // -------------------
+                }
+                    
+                else if translation.y >= 0 {
+                    if oldTranslation == +1 {
+                        Image.image = UIImage(named: "ChairChestDown")
+                    }
+                    oldTranslation = -1
+                    
+                    // --> Placeholder <--
+                    // Put Actuator Function for feet down here
+                    // -------------------
+                }
+            }
+                
+                //--------------------------------------------------------------------------
+                //-- Status: Ended --> Pan has ended. Reestablish starting-condition --
+            else if recognizer.state == UIGestureRecognizer.State.ended {
+                oldTranslation = 0 //set back to start-value
+                Image.image = UIImage(named: "ChairNormal")
+            }
+        }
+    }
+    
+    
     
     //----------------------------------------
     //----- generic remote design buttons ----
@@ -80,18 +215,6 @@ class RemoteController: UIViewController{
     }
     
     @IBAction func TourchBtn(_ sender: Any) {
-    }
-    
-    //---------------------------------------
-    //----- Fancy Design Remote Actions -----
-    
-    @IBAction func PresetsButton(_ sender: Any) {
-    }
-    
-    @IBAction func AddPresetsButton(_ sender: Any) {
-    }
-    
-    @IBAction func ExtraFunctions(_ sender: Any) {
     }
     
 }
