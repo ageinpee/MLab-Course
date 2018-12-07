0a5bdd4af194f109ce9eb726349aac9de58dc12c
Nima Rahrakhshan <Nima.Rahrakhshan@gmail.com>
2018-12-08 00:16:32 +0100

    Modularized (working!) Bluetooth

---
+++ XCode/DewertOkin/DewertOkin/Bluetooth/BluetoothCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  BluetoothCoordinator.swift
+//  BluetoothTest
+//
+//  Created by Nima Rahrakhshan on 05.12.18.
+//  Copyright © 2018 Nima Rahrakhshan. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+// Our Coordinator interface
+class BluetoothCoordinator {
+    
+    weak var bluetoothService = Bluetooth.sharedBluetooth
+    
+    init(bluetoothService: Bluetooth) {
+        self.bluetoothService = bluetoothService
+    }
+    
+    func scanStarted() { }
+    
+    func scanStopped() { }
+    
+    func connected(peripheral: CBPeripheral) { }
+    
+    func disconnected(failure: Bool) { }
+    
+    func bluetoothOff() { }
+    
+    func bluetoothOn() { }
+    
+    func discoveredPeripheral() { }
+    
+    func ableToWrite() { }
+    
+    func reconnect() { }
+}
