a9148a1a98feecf2f8df7088bc116f8924d299c9
Jan Robert <jan.janneck@outlook.com>
2018-12-08 18:27:08 +0100

    Removed the Core Animation progress bar from achievements for now

--- XCode/DewertOkin/DewertOkin/Settings and more/Achievements/AchievementsTableViewCell.swift
+++ XCode/DewertOkin/DewertOkin/Settings and more/Achievements/AchievementsTableViewCell.swift
@@ -15,47 +15,47 @@ class AchievementsTableViewCell: UITableViewCell {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var achievementImage: UIImageView!
     
-    let shapeLayer = CAShapeLayer()
+    //let shapeLayer = CAShapeLayer()
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        let center = CGPoint(x: 1.8 * self.frame.midX, y: self.frame.midY)
-        
-        let circularPath = UIBezierPath(arcCenter: center, radius: 35, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = UIColor.red.cgColor
-        shapeLayer.lineCap = .round
-        
-        let trackLayer = CAShapeLayer()
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.red.cgColor
-        trackLayer.lineCap = .round
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.lineWidth = 2
-        
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.lineWidth = 5
-        shapeLayer.strokeEnd = 0
-        
-        self.layer.addSublayer(trackLayer)
-        self.layer.addSublayer(shapeLayer)
-        
-        //achievementImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        handleTap()
+//        let center = CGPoint(x: 1.8 * self.frame.midX, y: self.frame.midY)
+//
+//        let circularPath = UIBezierPath(arcCenter: center, radius: 35, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
+//        shapeLayer.path = circularPath.cgPath
+//        shapeLayer.strokeColor = UIColor.red.cgColor
+//        shapeLayer.lineCap = .round
+//
+//        let trackLayer = CAShapeLayer()
+//        trackLayer.path = circularPath.cgPath
+//        trackLayer.strokeColor = UIColor.red.cgColor
+//        trackLayer.lineCap = .round
+//        trackLayer.strokeColor = UIColor.lightGray.cgColor
+//        trackLayer.fillColor = UIColor.clear.cgColor
+//        trackLayer.lineWidth = 2
+//
+//        shapeLayer.fillColor = UIColor.clear.cgColor
+//        shapeLayer.lineWidth = 5
+//        shapeLayer.strokeEnd = 0
+//
+//        self.layer.addSublayer(trackLayer)
+//        self.layer.addSublayer(shapeLayer)
+//
+//        //achievementImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+//        handleTap()
         
     }
     
-    @objc
-    private func handleTap() {
-        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        basicAnimation.toValue = 1
-        basicAnimation.duration = 2
-        basicAnimation.fillMode = .forwards
-        basicAnimation.isRemovedOnCompletion = false
-        shapeLayer.add(basicAnimation, forKey: "basicAnimation")
-    }
+//    @objc
+//    private func handleTap() {
+//        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+//        basicAnimation.toValue = 1
+//        basicAnimation.duration = 2
+//        basicAnimation.fillMode = .forwards
+//        basicAnimation.isRemovedOnCompletion = false
+//        shapeLayer.add(basicAnimation, forKey: "basicAnimation")
+//    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
