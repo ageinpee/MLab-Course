2455cc012da80df5f3fbd3efc4d22c4b69dd1d40
Nima Rahrakhshan <Nima.Rahrakhshan@gmail.com>
2018-12-26 12:11:13 +0100

    Adding Bluetooth Background Handler

---
+++ XCode/DewertOkin/DewertOkin/Bluetooth/BluetoothBackgroundHandler.swift
@@ -0,0 +1,21 @@
+//
+//  BluetoothBackgroundHandler.swift
+//  DewertOkin
+//
+//  Created by Nima Rahrakhshan on 26.12.18.
+//  Copyright © 2018 Team DewertOkin. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+class BluetoothBackgroundHandler {
+    
+    let remoteController = RemoteControlConfig()
+    let bluetooth = Bluetooth.sharedBluetooth
+    let bluetoothFlow = BluetoothFlow(bluetoothService: self.bluetooth)
+    
+    
+    
+    
+}
