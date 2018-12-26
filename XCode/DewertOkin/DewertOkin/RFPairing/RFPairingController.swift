104f6326c86a2d595a03ab6cfb2abd826db9d529
Henrik Peters <henrik.peters.98@web.de>
2018-12-26 09:27:55 +0100

    added csv reader

---
+++ XCode/DewertOkin/DewertOkin/RFPairing/RFPairingController.swift
@@ -0,0 +1,19 @@
+//
+//  RFPairingController.swift
+//  DewertOkin
+//
+//  Created by MacBook-Benutzer on 26.12.18.
+//  Copyright © 2018 Team DewertOkin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RFPairingController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
