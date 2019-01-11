47845d9ce7f4ee1494281c98c2c000494b69b5b1
Nima Rahrakhshan <Nima.Rahrakhshan@gmail.com>
2019-01-11 17:43:00 +0100

    Added dynamic current location into map view

--- XCode/DewertOkin/DewertOkin/Explore/ExploreViewController.swift
+++ XCode/DewertOkin/DewertOkin/Explore/ExploreViewController.swift
@@ -10,14 +10,33 @@ import Foundation
 import UIKit
 import MapKit
 
-class ExploreViewController: UIViewController, UITableViewDelegate {
+class ExploreViewController: UIViewController, CLLocationManagerDelegate ,UITableViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Devices"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        if let userLocation = locationManager.location?.coordinate {
+            let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 2000, longitudinalMeters: 2000)
+            mapView.setRegion(viewRegion, animated: true)
+            mapView.showsUserLocation = true
+        }
+        
+        DispatchQueue.main.async {
+            self.locationManager.startUpdatingLocation()
+        }
     }
 }
