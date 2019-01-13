71fb61149f8bc91978b3697c48733e6f1f556faa
Nima Rahrakhshan <Nima.Rahrakhshan@gmail.com>
2019-01-13 22:35:02 +0100

    Added Custom Accessory Cell, Reworked JSON File, Added Accessory Images

--- XCode/DewertOkin/DewertOkin/Explore/ExploreAccessoriesViewController.swift
+++ XCode/DewertOkin/DewertOkin/Explore/ExploreAccessoriesViewController.swift
@@ -16,15 +16,57 @@ class ExploreAccessoriesViewController: UIViewController, UITableViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationItem.title = "Accessories"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        initializeAccessories()
+        //tableView.beginUpdates()
     }
     
     func initializeAccessories() {
         let accessoryList = parseAccessories()
         guard accessoryList.count != 0 else { return }
-        for number in 0..<(accessoryList.count - 1) {
-            let accessorie = Accessory(imageName: accessoryList[number].imageName, name: accessoryList[number].name, accessoryDescription: accessoryList[number].accessoryDescription)
+        for number in 0..<(accessoryList.count) {
+            let accessory = Accessory(imageName: accessoryList[number].imageName, name: accessoryList[number].name, accessoryDescription: accessoryList[number].accessoryDescription)
+            accessoriesList.append(accessory)
+            tableView.reloadData()
         }
+        registerAccessories()
     }
+    
+    func registerAccessories() {
+        self.tableView.register(AccessoryCustomCell.self, forCellReuseIdentifier: "customAccessoryCell")
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 200
+    }
+}
+
+extension ExploreAccessoriesViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return accessoriesList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "customAccessoryCell") as! AccessoryCustomCell
+        let image = UIImage(named: accessoriesList[indexPath.row].imageName)
+        cell.accessoryImage = image
+        cell.accessoryName = accessoriesList[indexPath.row].name
+        cell.accessoryDescription = accessoriesList[indexPath.row].accessoryDescription
+        cell.layoutSubviews()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    
 }
