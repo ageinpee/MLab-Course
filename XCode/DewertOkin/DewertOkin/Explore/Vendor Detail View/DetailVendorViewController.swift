5904a3a68427f47442ae0032d17f01604be26f30
Nima Rahrakhshan <Nima.Rahrakhshan@gmail.com>
2019-01-15 14:48:49 +0100

    Added Tap Gesture for background

--- XCode/DewertOkin/DewertOkin/Explore/Vendor Detail View/DetailVendorViewController.swift
+++ XCode/DewertOkin/DewertOkin/Explore/Vendor Detail View/DetailVendorViewController.swift
@@ -17,18 +17,19 @@ class DetailVendorViewController: UIViewController {
     
     lazy var backgroundAlphaView: UIView = {
         let view = UIView(frame: self.view.bounds)
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        view.backgroundColor = .clear //UIColor.black.withAlphaComponent(0)
         return view
     }()
     let vendorView = UIView()
     var isPresenting = false
     
     var panGestureForView = UIPanGestureRecognizer()
+    var panGestureForMap = UITapGestureRecognizer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear
-        //view.addSubview(backgroundAlphaView)
+        view.addSubview(backgroundAlphaView)
         view.addSubview(vendorView)
         initializeVendorInformation()
     }
@@ -56,14 +57,22 @@ class DetailVendorViewController: UIViewController {
         panGestureForView = UIPanGestureRecognizer(target: self, action: #selector(DetailVendorViewController.draggedView(_:)))
         vendorView.isUserInteractionEnabled = true
         vendorView.addGestureRecognizer(panGestureForView)
+        
+        panGestureForMap = UITapGestureRecognizer(target: self, action: #selector(DetailVendorViewController.closeVendorDetail(_:)))
+        backgroundAlphaView.isUserInteractionEnabled = true
+        backgroundAlphaView.addGestureRecognizer(panGestureForMap)
     }
     
     @objc func draggedView(_ sender: UIPanGestureRecognizer) {
         self.view.bringSubviewToFront(vendorView)
         let transition = sender.translation(in: self.view)
-        guard (vendorView.center.y <= self.view.frame.height / 2) && transition.y > 0 else { return }
+        //guard (vendorView.center.y <= self.view.frame.height / 2) && transition.y > 0 else { return }
         vendorView.center = CGPoint(x: vendorView.center.x, y: vendorView.center.y + transition.y)
         sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    @objc func closeVendorDetail(_ sender: UIPanGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
     }
 }
 
@@ -90,11 +99,11 @@ extension DetailVendorViewController: UIViewControllerTransitioningDelegate {
         if isPresenting == true {
             containerView.addSubview(toVC.view)
             
-            vendorView.frame.origin.y += self.view.frame.height / 2
+            vendorView.frame.origin.y += self.view.frame.height / 3
             backgroundAlphaView.alpha = 0
             
             UIView.animate(withDuration: 0.4, delay: 0, options: [.curveEaseOut], animations: {
-                self.vendorView.frame.origin.y -= self.view.frame.height / 2
+                self.vendorView.frame.origin.y -= self.view.frame.height / 3
                 self.backgroundAlphaView.alpha = 1
             }, completion: { (finished) in
                 transitionContext.completeTransition(true)
