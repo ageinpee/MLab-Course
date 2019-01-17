2ed2903551ebc3d9fcaa6fe044511b69183ea4ab
Jan Robert <jan.janneck@outlook.com>
2019-01-17 10:34:29 +0100

    Added placeholder tabbaritem for Explore

--- XCode/DewertOkin/DewertOkin/MainViewController.swift
+++ XCode/DewertOkin/DewertOkin/MainViewController.swift
@@ -45,6 +45,7 @@ class MainViewController: UITabBarController {
         healthVC.tabBarItem = UITabBarItem(title: "Companion", image: UIImage(named: "heart_icon_outlined"), selectedImage: UIImage(named: "heart_icon_selected"))
         
         exploreVC = explore
+        exploreVC.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 2)
         
         settingsVC = settings
         settingsVC.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 3)
