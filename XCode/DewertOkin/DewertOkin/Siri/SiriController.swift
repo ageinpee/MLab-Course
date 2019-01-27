2e147c8a7cbad0d9000cb91f10b8f6d981692c9b
Nima Rahrakhshan <Nima.Rahrakhshan@gmail.com>
2019-01-27 09:15:59 +0100

    Working Shortcut Center

--- XCode/DewertOkin/DewertOkin/Siri/SiriController.swift
+++ XCode/DewertOkin/DewertOkin/Siri/SiriController.swift
@@ -13,18 +13,15 @@ import Intents
 
 extension RemoteController {
     
-    func setNewActivity(activityType: String, title: String) {
+    @available(iOS 12.0, *)
+    func setNewActivity(activityType: String, title: String) -> INShortcut {
         let activity = NSUserActivity(activityType: activityType)
         activity.title = title
         activity.isEligibleForSearch = true
-        if #available(iOS 12.0, *) {
-            activity.isEligibleForPrediction = true
-            activity.persistentIdentifier = NSUserActivityPersistentIdentifier(activityType)
-        } else {
-            print("Update your device dude, dafuq!")
-        }
-        view.userActivity = activity
-        activity.becomeCurrent()
+        activity.isEligibleForPrediction = true
+        activity.persistentIdentifier = NSUserActivityPersistentIdentifier(activityType)
+        
+        return INShortcut(userActivity: activity)
         
         //getCommandToTrigger(command: activity.title!)
     }
@@ -42,21 +39,44 @@ extension RemoteController {
     }
     
     func initializeAllCommands() {
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.startHeadUp", title: "Move Head Up")
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.startHeadDown", title: "Move Head Down")
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.startFeetUp", title: "Move Feet Up")
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.startFeetDown", title: "Move Feet Down")
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.triggerMemory1", title: "Trigger Memory 1")
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.triggerMemory2", title: "Trigger Memory 2")
-        
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.stopHeadUp", title: "Stop Head Up")
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.stopHeadDown", title: "Stop Head Down")
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.stopFeetUp", title: "Stop Feet Up")
-        setNewActivity(activityType: "de.uhh.mlabdewertokin.stopFeetDown", title: "Stop Feet Down")
+        if #available(iOS 12.0, *) {
+            var shortcuts = [INShortcut]()
+            
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.startHeadUp", title: "Move Head Up"))
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.startHeadDown", title: "Move Head Down"))
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.startFeetUp", title: "Move Feet Up"))
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.startFeetDown", title: "Move Feet Down"))
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.triggerMemory1", title: "Trigger Memory 1"))
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.triggerMemory2", title: "Trigger Memory 2"))
+            
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.stopHeadUp", title: "Stop Head Up"))
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.stopHeadDown", title: "Stop Head Down"))
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.stopFeetUp", title: "Stop Feet Up"))
+            shortcuts.append(setNewActivity(activityType: "de.uhh.mlabdewertokin.stopFeetDown", title: "Stop Feet Down"))
+            
+            INVoiceShortcutCenter.shared.setShortcutSuggestions(shortcuts)
+            
+        } else {
+            print("Update your device dude, dafuq!")
+        }
     }
     
     func intermediateHelper() {
         print("it works")
     }
+    
+//    func initializeAllCommands() {
+//        setNewActivity(activityType: "de.uhh.mlabdewertokin.startHeadUp", title: "Move Head Up")
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.startHeadDown", title: "Move Head Down") })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.startFeetUp", title: "Move Feet Up") })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.startFeetDown", title: "Move Feet Down") })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 4.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.triggerMemory1", title: "Trigger Memory 1") })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 5.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.triggerMemory2", title: "Trigger Memory 2") })
+//
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 6.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.stopHeadUp", title: "Stop Head Up") })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 7.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.stopHeadDown", title: "Stop Head Down") })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 8.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.stopFeetUp", title: "Stop Feet Up") })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 9.5, execute: { self.setNewActivity(activityType: "de.uhh.mlabdewertokin.stopFeetDown", title: "Stop Feet Down") })
+//    }
     
 }
